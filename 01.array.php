a5cceeb9e03021b249045260a606cf2931c49868
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-05-14 18:01:44 -0300

    exibe_lista_for

--- 01.array.php
+++ 01.array.php
@@ -1,8 +1,9 @@
 <?php
 //Array é uma forma de agrupar dados em uma unica variavel
+$id = [0,1,2,3,4,5,6];
 $idadeList = [21, 23, 19, 25, 30, 41, 18];
 $nomes = ["Juliano","Alan","Maria","João","Mario","Marcos","Gabriel"];
 
-for ($i=1; $i<=6; $i++){
-    echo $nomes[$i]." - ".$idadeList[$i].PHP_EOL;
+for ($i=0; $i < count($id); $i++){
+    echo $id[$i]. " \t".$nomes[$i]." \t ".$idadeList[$i].PHP_EOL;
 }
