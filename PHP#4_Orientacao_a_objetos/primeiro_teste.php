9bc9e6e08858b2ca9b637f1ef1124fb318b2b161
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-06-03 18:54:02 -0300

    orientação a objeto com PHP

---
+++ PHP#4_Orientacao_a_objetos/primeiro_teste.php
@@ -0,0 +1,9 @@
+<?php
+require 'src/conta.php';
+$conta = criarConta('123.456.789-10', 'Vinicius Dias', 500);
+var_dump($conta); // Observe que a conta foi criada corretamente
+$conta['123.456.789-10']['saldo'] -= 700;
+var_dump($conta); // Verifique que uma nova conta foi criada de forma incorreta
+
+$conta = criarConta('090.923.066-81','Juliano Silva',600);
+var_dump($conta);
