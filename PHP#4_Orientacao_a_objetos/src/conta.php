a709964a97b2d1ff8e331b4306d49932098138b4
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-06-04 18:03:32 -0300

    POO - Instanciar

--- PHP#4_Orientacao_a_objetos/src/conta.php
+++ PHP#4_Orientacao_a_objetos/src/conta.php
@@ -6,5 +6,35 @@ class Conta
     //Precisamos abstrrai o que precisamos para nosso sistema
     public string $cpfTitular; //estas variaveis são atributos de conta
     public string $nomeTitular;
-    public float $saldo;
+    public float $saldo = 0;
+
+    //Sacar
+    public function sacar(float $ValorASacar)
+    {
+        if ($ValorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+        $this->saldo -= $ValorASacar;
+    }
+
+    //Depositar
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+    //Transferir
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+            $this->sacar($valorATransferir);
+            $contaDestino->depositar($valorATransferir);
+    }
 }
