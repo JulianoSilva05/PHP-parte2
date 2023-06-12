bc15069df6e00ba42a5f7f7087354750f809fe23
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-06-12 10:07:45 -0300

    funções

---
+++ PHP#5_Funções/01.intro.php
@@ -0,0 +1,13 @@
+<?php
+//codigo
+//Funções Build IN exemplo:
+//strlen, strtoupper, strtolower, print_r, var_dump....etc
+function teste(){
+    $a = 4;
+    $b = 2;
+    echo $a + $b;
+}
+teste().PHP_EOL;
+
+echo "Função STRTOUPPER"; 
+echo strtoupper("Olá, Mundo!");
