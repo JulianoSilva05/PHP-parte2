e5042c650773e43326a547ca82811ae2c2498388
Juliano Silva <julianoqm@gmail.com>
2023-06-13 15:18:44 -0300

    POO

---
+++ PHP#4_Orientacao_a_objetos/intro.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * O que são objetos?
+ * AS características são conhecidas como propriedades (variáveis)
+ * Os comportamentos como métodos(funções);
+ * Os objetos interagem entre si e sistemas são escritos orientados a objetos(paradigma de Orientação a Objetos)
+ * No PHP podemos desenvolver neste paradigma.
+ */
