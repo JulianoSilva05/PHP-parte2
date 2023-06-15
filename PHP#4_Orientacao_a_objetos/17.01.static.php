fd0b1d4afc3a0a1b42e2c9c80153c763f850b9d4
Juliano Silva <julianoqm@gmail.com>
2023-06-15 16:42:30 -0300

    static

--- PHP#4_Orientacao_a_objetos/17.01.static.php
+++ PHP#4_Orientacao_a_objetos/17.01.static.php
@@ -6,8 +6,16 @@
 class Login{
     public static $user;
     public static function verificaLogin(){
-        echo "O usuario ".self::$user." está logado";//$this não funciona em metodos estaticos
+        echo "O usuario ".self::$user." está logado<br />";//$this não funciona em metodos estaticos
+    }
+    public function sairSistema(){
+        echo "O usuário saiu do sistema<br />";
     }
 }
 Login::$user = "Juliano";
 Login::verificaLogin(); //Declara o nome da Classe, :: e o metodo que deseja chamar.
+
+
+//Criar metodos e atributos ESTATICOS não significa que não poderá instanciar objetos
+$juliano_fernando = new Login();
+$juliano_fernando -> sairSistema();
