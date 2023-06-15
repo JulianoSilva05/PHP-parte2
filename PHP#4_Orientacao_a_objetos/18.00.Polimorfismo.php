03cd68b730658d5a790c4644516d3efbd261d00f
Juliano Silva <julianoqm@gmail.com>
2023-06-15 16:45:08 -0300

    polimosfismo

---
+++ PHP#4_Orientacao_a_objetos/18.00.Polimorfismo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * O polimorfismo é um dos princípios fundamentais da Programação Orientada a Objetos (POO). Ele permite que objetos de diferentes classes sejam tratados de maneira uniforme, ou seja, é a capacidade de um objeto assumir diferentes formas, comportando-se de maneiras diferentes, dependendo do contexto.
+ */
+/**
+ * O polimorfismo ocorre quando uma classe pode ser referenciada através de uma classe base, mas a execução de um método é determinada pelo tipo real do objeto em tempo de execução.
+ */
+
+ 
